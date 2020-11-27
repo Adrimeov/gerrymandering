a5a7d03298a1171296ba852f899acd596a5801a2
adrimeov <jffontaine_@hotmail.com>
2020-11-26 19:51:42 -0500

    swapping

--- cpp_project/main.cpp
+++ cpp_project/main.cpp
@@ -3,6 +3,7 @@
 #include "list"
 #include "cassert"
 #include "math.h"
+#include <queue>
 
 using namespace std;
 
@@ -11,7 +12,12 @@ struct Municipality {
     int y;
     int coadjacency_index;
     int votes;
-
+    Municipality(const Municipality &municipality_to_copy){
+        x = municipality_to_copy.x;
+        y = municipality_to_copy.y;
+        coadjacency_index =  municipality_to_copy.coadjacency_index;
+        votes = municipality_to_copy.votes;
+    }
     Municipality(int _x, int _y, int _votes): x(_x), y(_y), votes(_votes), coadjacency_index(0) {};
 };
 
@@ -20,16 +26,20 @@ struct District {
     int vote_cost;
     vector<Municipality> municipalities;
 
+    District(const District &district_to_copy){
+        distance_max = district_to_copy.distance_max;
+        vote_cost = district_to_copy.vote_cost;
+        municipalities = vector<Municipality>(district_to_copy.municipalities);
+    }
     District(): distance_max(0), vote_cost(0) {
         municipalities = vector<Municipality>();
     };
 };
 
 struct State {
-
     float distance_cost;
     float vote_cost;
-    list<District> districts;
+    vector<District> districts;
     // TODO: L'enlever de la??
     vector<Municipality> municipalities;
     int **coadjacency_matrix;
@@ -37,16 +47,32 @@ struct State {
     int nb_cols;
     int nb_municipalities;
 
+    State(const State &state_to_copy) {
+        distance_cost = state_to_copy.distance_cost;
+        vote_cost = state_to_copy.vote_cost;
+        districts = vector<District>(state_to_copy.districts);
+        municipalities = vector<Municipality>(state_to_copy.municipalities);
+        coadjacency_matrix = state_to_copy.coadjacency_matrix;
+        nb_municipalities = state_to_copy.nb_municipalities;
+        nb_rows = state_to_copy.nb_rows;
+        nb_cols = state_to_copy.nb_cols;
+    }
+
     State(const vector<Municipality> &municipalities_, int rows, int cols, int nb_district): nb_rows(rows), nb_cols(cols), distance_cost(0), vote_cost(0) {
         nb_municipalities = municipalities_.size();
         municipalities = municipalities_;
-        districts = list<District>(nb_district);
+        districts = vector<District>(nb_district);
         // TODO: maybe shuffle les municipalites
+
+        queue<int> fifo;
+        for(int i = 0; i < nb_district; i++)
+            fifo.push(i);
+
         for(auto itr : municipalities) {
-            District district = districts.front();
-            districts.pop_front();
-            district.municipalities.push_back(itr);
-            districts.push_back(district);
+            int district = fifo.front();
+            fifo.pop();
+            fifo.push(district);
+            districts[district].municipalities.push_back(itr);
         }
         Setup_Coadjacency();
     };
@@ -69,11 +95,32 @@ struct State {
 };
 
 
-void ShowState(const State &state) {
-    for(auto itr: state.districts) {
 
+void ShowState(const State &state) {
+    int matrice[state.nb_rows][state.nb_cols];
+    int index = 1;
+    for(auto itr = state.districts.begin(); itr != state.districts.end(); itr++, index++) {
+        for(auto itr_2 = itr->municipalities.begin(); itr_2 != itr->municipalities.end(); itr_2++)
+            matrice[itr_2->x][itr_2->y] = index;
     }
+    for(int i = 0; i < state.nb_rows; i++) {
+        for (int j = 0; j < state.nb_cols; j++)
+            cout << matrice[i][j] << " ";
+        cout << endl;
+    }
+
 }
+
+State swap_municipalities(const State current_state, int dist_idx_1, int dist_idx_2, int mun_idx_1, int mun_idx_2){
+    State new_state = State(current_state);
+    // TODO essayer de swap les ref, pas les objets.
+    Municipality municipality_tempo = new_state.districts[dist_idx_1].municipalities[mun_idx_1];
+    new_state.districts[dist_idx_1].municipalities[mun_idx_1] = new_state.districts[dist_idx_2].municipalities[mun_idx_2];
+    new_state.districts[dist_idx_2].municipalities[mun_idx_2] = municipality_tempo;
+    return new_state;
+
+}
+
 
 
 int main() {
@@ -111,6 +158,10 @@ int main() {
         assert(itr.municipalities.size() == min_municipalities_per_district || itr.municipalities.size() == max_municipalities_per_district);
     }
 
+    ShowState(test_state_1);
+    State new_state = swap_municipalities(test_state_1, 0, 1, 0, 0);
+    ShowState(new_state);
+//    State swap_municipalities(const State current_state, int dist_idx_1, int dist_idx_2, int mun_idx_1, int mun_idx_2){
 
-    return 0;
+
 }
