670fbc026d9ec034a64dbe253ec12b02702ff2d5
polarbuzzle <fersamw@gmail.com>
2020-11-29 11:54:29 -0500

    Simple reader implemented and tested

---
+++ cpp_project/Municipality.cpp
@@ -0,0 +1,39 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+struct Municipality {
+    int x;
+    int y;
+    int coadjacency_index;
+    int votes;
+    Municipality(const Municipality &municipality_to_copy){
+        x = municipality_to_copy.x;
+        y = municipality_to_copy.y;
+        coadjacency_index =  municipality_to_copy.coadjacency_index;
+        votes = municipality_to_copy.votes;
+    }
+    Municipality(int _x, int _y, int _votes): x(_x), y(_y), votes(_votes), coadjacency_index(0) {};
+
+    int get_x() { return x; }
+    int get_y() { return y; }
+    int get_votes() { return votes; }
+
+    void set_x(int value) { x = value; }
+    void set_y(int value) { y = value; }
+    void set_votes(int value) { votes = value; }
+
+};
+
+PYBIND11_MODULE(Municipality, m) {
+
+    py::class_<Municipality>(m, "Municipality")
+            .def(py::init<int, int, int>())
+            .def("get_x", &Municipality::get_x)
+            .def("get_y", &Municipality::get_y)
+            .def("get_votes", &Municipality::get_votes)
+            .def("set_x", &Municipality::set_x)
+            .def("set_y", &Municipality::set_y)
+            .def("set_votes", &Municipality::set_votes);
+}
--- sample_reader.py
+++ sample_reader.py
@@ -1,4 +1,5 @@
 import numpy as np
+from cpp_project import Municipality
 
 DATA_DIR = "./exemplaires"
 
@@ -9,21 +10,33 @@ def read_samples(path):
     with open(path, "r") as file:
         x, y = [int(i) for i in file.readline().split()]
 
-        for line in file:
+        for i, line in enumerate(file):
             line = line.replace("\n", "")
             row = np.array([int(i) for i in line.split()])
-            municipalities = np.concatenate((municipalities, row))
+
+            row_municipalities = [Municipality.Municipality(j, i, votes)
+                                  for j, votes in enumerate(row)]
+
+            municipalities = np.concatenate((municipalities, row_municipalities))
 
     return x, y, municipalities
 
 
 if __name__ == "__main__":
-
     filename = f"{DATA_DIR}/test_samples.txt"
-
-    x, y, samples = read_samples(filename)
+    x, y, municipalities = read_samples(filename)
 
     assert x == 6
     assert y == 7
-    assert samples.size == x * y
+    assert municipalities.size == x * y
 
+    municipality_1 = municipalities[0]
+    municipality_2 = municipalities[-1]
+
+    assert municipality_1.get_x() == 0
+    assert municipality_1.get_y() == 0
+
+    a = municipality_2.get_y()
+
+    assert municipality_2.get_x() == x - 1
+    assert municipality_2.get_y() == y - 1
