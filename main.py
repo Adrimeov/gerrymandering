d52114baa335813f1853a7e00b2ba60941567fc8
polarbuzzle <fersamw@gmail.com>
2020-11-29 21:24:40 -0500

    Trying to call local search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import sys
+import sample_reader
+from cpp_project import CppLib
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
@@ -15,4 +17,9 @@ if __name__ == "__main__":
     args_as_dict = vars(args)
     parameters = {a: vars(args)[a] for a in vars(args) if vars(args)[a] != ""}
 
-    print(parameters)
+    samples_path = parameters["path"]
+    nb_districts = int(parameters["nb_districts"])
+
+    x, y, municipalities = sample_reader.read_samples(samples_path)
+    state = CppLib.Valid_State_Local_Search(municipalities, y, x, nb_districts)
+
